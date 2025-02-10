7951c7e34d074c255382e424fe4aea585a57549c
YiCyun Chen <kelly@chengchitech.com>
2025-02-10 18:58:49 +0800

    [feat] add user table

--- src/models.py
+++ src/models.py
@@ -1,5 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    account: Mapped[str] = mapped_column(String(50), unique=True)
+    password: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(String(255), nullable=True)
+    role: Mapped[int]
+    phone: Mapped[str] = mapped_column(nullable=True)
